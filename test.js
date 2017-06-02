3ecff0cd7d298c8bfe9566b9918794df27ba1051
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-02 03:15:05 -0400

    run update, lint

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
  * git-branch <https://github.com/jonschlinkert/git-branch>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
@@ -10,16 +10,20 @@
 var assert = require('assert');
 var branch = require('./');
 
-describe('async', function () {
-  it('should return the current git branch:', function () {
-    branch(function (err, res) {
-      assert(res === 'master');
+describe('git-branch', function() {
+  describe('async', function() {
+    it('should return the current git branch', function(cb) {
+      branch(function(err, res) {
+        if (err) return cb(err);
+        assert.equal(res, 'master');
+        cb();
+      });
     });
   });
-});
 
-describe('sync', function () {
-  it('should return the current git branch:', function () {
-    assert(branch.sync() === 'master');
+  describe('sync', function() {
+    it('should return the current git branch', function() {
+      assert.equal(branch.sync(), 'master');
+    });
   });
 });
